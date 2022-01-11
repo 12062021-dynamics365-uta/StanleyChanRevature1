247529de89e0aec198ed624f6525c61b30f324b4
Your Name <stanley.chan@revature.net>
2022-01-10 20:08:26 -0600

    aidez moi s'il vous plait

--- SweetnSaltyAPI-Assignment/SweetnSaltyAPI/SweetnSaltyBusiness/ISweetnSaltyBusinessClass.cs
+++ SweetnSaltyAPI-Assignment/SweetnSaltyAPI/SweetnSaltyBusiness/ISweetnSaltyBusinessClass.cs
@@ -12,5 +12,14 @@ namespace SweetnSaltyBusiness
         Task<Flavor> PostFlavor(string flavor);
 
         Task<Person> PostPerson(string fName, string lName);
+
+        //get person by name
+        Task<Person> GetPersonByName(string fName, string lName);
+
+        //get person and liked flavors by id
+        Task<Person> GetPersonAndFlavorsByID(int personID);
+
+        //get a list of flavors available
+        Task<List<Flavor>> GetAllFlavors();
     }
 }
